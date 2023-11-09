f04a3e13878fa11c208188d367e4d956cf002866
Marcell Misó <misomarcell@gmail.com>
2023-11-09 18:37:58 +0100

    Add name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { Component, HostBinding, inject } from "@angular/core";
 import {
 	Database,
 	get,
@@ -13,18 +13,34 @@ import {
 	FormGroup,
 	FormsModule,
 	ReactiveFormsModule,
+	Validators,
 } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { ViewEncapsulation } from "@angular/compiler";
+import { MatRippleModule } from "@angular/material/core";
 
 interface Message {
 	id: string;
 	message: string;
+	name: string;
 }
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterOutlet],
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		RouterOutlet,
+		MatInputModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatRippleModule,
+	],
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"],
 })
@@ -33,11 +49,12 @@ export class AppComponent {
 	database: Database = inject(Database);
 
 	form: FormGroup = new FormGroup({
-		message: new FormControl(""),
+		message: new FormControl("", Validators.required),
 	});
 
 	private messagesRef = ref(this.database, `messages`);
 
+	@HostBinding("class.app-component") hostClass = true;
 	constructor() {}
 
 	async ngOnInit() {
@@ -55,8 +72,23 @@ export class AppComponent {
 	}
 
 	async onSubmit() {
-		const newMessage = { message: this.form.value.message } as Message;
+		if (this.form.invalid) return;
+
+		const newMessage = {
+			message: this.form.value.message,
+			name: this.getName(),
+		} as Message;
 		await push(this.messagesRef, newMessage);
 		this.form.reset();
+	}
+
+	private getName(): string {
+		const localStorageName = localStorage.getItem("name");
+		if (localStorageName) return localStorageName;
+
+		const name = Math.random().toString(36).substring(7);
+		localStorage.setItem("name", name);
+
+		return name;
 	}
 }
