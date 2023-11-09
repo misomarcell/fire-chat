71965676e03920b20aea1e4a19abd0188780f417
Marcell Misó <misomarcell@gmail.com>
2023-11-09 18:05:23 +0100

    Add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,31 +14,28 @@ import { provideClientHydration } from "@angular/platform-browser";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideRouter(routes),
-		provideClientHydration(),
-		importProvidersFrom(
-			provideFirebaseApp(() =>
-				initializeApp({
-					projectId: "marcell-fire-chat",
-					appId: "1:458117046067:web:fdbe93b2040358d2f8a846",
-					databaseURL:
-						"https://marcell-fire-chat-default-rtdb.europe-west1.firebasedatabase.app",
-					storageBucket: "marcell-fire-chat.appspot.com",
-					apiKey: "AIzaSyAnpfnTbQzmU3Ms7fviLZb8Rmj_n3ad8BU",
-					authDomain: "marcell-fire-chat.firebaseapp.com",
-					messagingSenderId: "458117046067",
-				})
-			)
-		),
-		importProvidersFrom(provideAuth(() => getAuth())),
-		importProvidersFrom(provideAnalytics(() => getAnalytics())),
-		ScreenTrackingService,
-		UserTrackingService,
-		importProvidersFrom(provideDatabase(() => getDatabase())),
-		importProvidersFrom(provideFunctions(() => getFunctions())),
-		importProvidersFrom(providePerformance(() => getPerformance())),
-	],
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+        projectId: "marcell-fire-chat",
+        appId: "1:458117046067:web:fdbe93b2040358d2f8a846",
+        databaseURL: "https://marcell-fire-chat-default-rtdb.europe-west1.firebasedatabase.app",
+        storageBucket: "marcell-fire-chat.appspot.com",
+        apiKey: "AIzaSyAnpfnTbQzmU3Ms7fviLZb8Rmj_n3ad8BU",
+        authDomain: "marcell-fire-chat.firebaseapp.com",
+        messagingSenderId: "458117046067",
+    }))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+    UserTrackingService,
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    importProvidersFrom(provideFunctions(() => getFunctions())),
+    importProvidersFrom(providePerformance(() => getPerformance())),
+    provideAnimations()
+],
 };
